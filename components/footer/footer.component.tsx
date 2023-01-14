1aff2821c08b878df1f3fd59d16c93aceff82720
metablvk <metablvk@gmail.com>
2023-01-14 15:01:04 -0700

    Comments

--- components/footer/footer.component.tsx
+++ components/footer/footer.component.tsx
@@ -1,6 +1,11 @@
-import styles from './footer.module.css';
+import styles from "./footer.module.css";
 
 const Footer = () => {
+  /**
+   * Footer Component
+   * @typedef {Props}
+   * A basic footer component
+   */
   return (
     <div className={styles.footer_container}>
       <footer className={`${styles.footer} container`}>
