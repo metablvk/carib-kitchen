ff8e22cec09c5ec206498e2086fe10ace4081153
metablvk <metablvk@gmail.com>
2023-01-14 14:54:37 -0700

    Added in layout comments.

--- components/layout/layout.component.tsx
+++ components/layout/layout.component.tsx
@@ -1,12 +1,20 @@
-import { FC, ReactNode } from 'react';
-import Footer from '../footer/footer.component';
-import Navbar from '../navbar/navbar.component';
-import styles from './layout.module.css';
+import { FC, ReactNode } from "react";
+import Footer from "../footer/footer.component";
+import Navbar from "../navbar/navbar.component";
+import styles from "./layout.module.css";
+
 type Props = {
   children: ReactNode;
 };
 
 const Layout: FC<Props> = ({ children }) => (
+  /**
+   * Layout Component
+   * @typedef {Props}
+   * @params {object} - children that get passed into the layout component
+   * A component that renders a navbar up top, and applies appropriate styling
+   * to the current layout of the page.
+   */
   <div className={`${styles.site}`}>
     <Navbar />
     <main className={styles.site_content}>{children}</main>
